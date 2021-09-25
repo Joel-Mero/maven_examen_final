97443230c9991201ec88d4d984192f00e2825d8b
Joel Mero <alejomero1@gmail.com>
2021-09-24 19:48:12 -0500

    Examen Final

---
+++ src/examen/final/maven_examen_final_aseguradora/MainAseguradora.java
@@ -0,0 +1,52 @@
+package maven_examen_final_aseguradora;
+
+import java.util.Scanner;
+
+public class MainAseguradora {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner teclado = new Scanner(System.in);
+		Scanner teclado1 = new Scanner(System.in);
+		
+		int opcion;
+		do {
+			System.out.println("-----ASEGURADORA DEL SUR-----");
+			System.out.println("1. Ingresar Póliza");
+			System.out.println("2. Actualizar Valor");
+			System.out.println("3. Consultar Póliza");
+			System.out.println("4. Imprima Reposrte");
+			System.out.println("5. SALIR");
+			opcion = teclado.nextInt();
+			
+			if (opcion == 1) {
+				System.out.println("-----Ecoga el tipp de Póliza-----");
+				System.out.println("a. Póliza General");
+				System.out.println("b. Póliza Vip");
+				String poliza = teclado1.nextLine();
+				
+				if(poliza.equals("a")) {
+					System.out.println("Ingrese Nombre");
+					String nombre = teclado1.nextLine();
+					System.out.println("Ingrese descripción");
+					String descripcion = teclado1.nextLine();
+					System.out.println("Ingrese Año vigencia");
+					String anioVigencia = teclado1.nextLine();
+					System.out.println("Ingrese Código de Póliza");
+					String codigoPoliza = teclado1.nextLine();
+					System.out.println("ingrese Valor de la Póliza");
+					int valorPoliza = teclado.nextInt();
+					System.out.println("Ingrese Código descuento");
+					String codigoDescuento = teclado1.nextLine();
+					System.out.println("Ingrese Número de Seguro");
+					String numeroSeguro = teclado1.nextLine();
+				}
+				
+				
+			}
+			
+		}while(opcion != 5);
+	}
+
+}
